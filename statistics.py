8f7e15d3634a5575bd986507582243a7f8660ba0
Guillermo Rodriguez <grodriguez@senegence.com>
2018-09-18 12:32:49 -0700

    Addendums + file name changes

---
+++ statistics.py
@@ -0,0 +1,46 @@
+"""
+    @ Author:       Guillermo Rodriguez
+    @ Date:         09/18/2018
+    @ Purpose:      Apply tokenization, lemmatization, and stemming to a data set
+"""
+
+# pip install -U nltk
+from nltk.stem import WordNetLemmatizer
+
+class statistics:
+
+    def __init__(self):
+        print('Statistics Object Initialized')
+        self.tokens = {}
+        self.lems = {}
+        self.stem = {}
+
+    """
+        @ Author:       Guillermo Rodriguez
+        @ Date:         09/18/2018
+        @ Purpose:      Tokenize a data set
+    """
+    def tokenization(self, data):
+        result = {}
+
+        return result
+
+    """
+            @ Author:       Guillermo Rodriguez
+            @ Date:         09/18/2018
+            @ Purpose:      Tokenize a data set
+        """
+    def lemmatization(self, data):
+        result = {}
+
+        return result
+
+    """
+            @ Author:       Guillermo Rodriguez
+            @ Date:         09/18/2018
+            @ Purpose:      Tokenize a data set
+        """
+    def stemminization(self, data):
+        result = {}
+
+        return result
--- tfidf.py
+++ tfidf.py
@@ -1,14 +1,38 @@
 import argparse
+import os
+from graph import *
+from statistics import *
 
 print('Started ....')
 
 parser = argparse.ArgumentParser(prog='tfidf.py')
 parser.add_argument('-graph', help='Graphical Data Output [NO | YES]')
 parser.add_argument('-statistics', help='Statistical Data Output [NO | YES]')
+parser.add_argument('-query', help='Query String')
 parse = parser.parse_args()
 
 if parse.graph and parse.statistics:
 
+    _path = os.getcwd() + '\\Data\\'
+    _type = '.txt'
+    _skip_first = True
+    _content = {}
+
+    # Read Data set
+    for _file in os.listdir(_path):
+        if _type in _file:
+            with open(_path + _file) as _from:
+                for _line in _from:
+                    # Company Description Detail in Second Column of Data Input
+                    _content[_file.replace(_type, '')] += (_line.split('\t')[1]) + ' '
+
+
+    print(_content)
+
+    # Get Statistics
+    _statistics = statistics()
+
+    # Create Graphical Elements
 
 else:
     parser.print_help()
