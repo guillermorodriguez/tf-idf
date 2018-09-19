1ece99fdae161830c42fd816db07b9fd8f9f51e3
Guillermo Rodriguez <grodriguez@senegence.com>
2018-09-18 23:43:24 -0700

    Addendums tf-idf

--- statistics.py
+++ statistics.py
@@ -1,7 +1,8 @@
 """
     @ Author:       Guillermo Rodriguez
     @ Date:         09/18/2018
-    @ Purpose:      Apply tokenization, lemmatization, and stemming to a data set
+    @ Purpose:      Apply tokenization, lemmatization, and stemming to a data set.
+                    Common point for the calculation of TF / IDF statistics.
     @ Dependency:   NLTK
                         python
                         >>> import nltk
@@ -13,6 +14,7 @@
 # nltk.download()
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
+import math
 
 class statistics:
 
@@ -21,6 +23,46 @@ class statistics:
     """
     def __init__(self):
         print('Statistics Object Initialized')
+
+    """
+        @ Author:       Guillermo Rodriguez
+        @ Date:         09/18/2018
+        @ Purpose:      Lemmatization a data set
+    """
+    def lemmatization(self, data):
+        result = {}
+
+        lemmatizer = WordNetLemmatizer()
+
+        for _token in data.split(' '):
+            _word = lemmatizer.lemmatize(_token)
+
+            if _word in result:
+                result[_word] += 1
+            else:
+                result[_word] = 1
+
+        return result
+
+    """
+        @ Author:       Guillermo Rodriguez
+        @ Date:         09/18/2018
+        @ Purpose:      Steminization a data set
+    """
+    def stemminization(self, data):
+        result = {}
+
+        stemmer = PorterStemmer()
+
+        for _token in data.split(' '):
+            _word = stemmer.stem(_token)
+
+            if _word in result:
+                result[_word] += 1
+            else:
+                result[_word] = 1
+
+        return result
 
     """
         @ Author:       Guillermo Rodriguez
@@ -43,39 +85,42 @@ class statistics:
     """
         @ Author:       Guillermo Rodriguez
         @ Date:         09/18/2018
-        @ Purpose:      Tokenize a data set
+        @ Purpose:      Calculate term frequency (tf)
     """
-    def lemmatization(self, tokenized_data):
-        result = {}
+    def tf(self, entries, word):
+        total = 0
+        length = 0
+        _root_word = PorterStemmer().stem(WordNetLemmatizer().lemmatize(word))
 
-        lemmatizer = WordNetLemmatizer()
+        for entry in entries:
+            length += len(entry)
+            if entry == _root_word:
+                total += 1
 
-        for _token in tokenized_data:
-            _word = lemmatizer.lemmatize(_token)
+        if length > 0:
+            return total / length
 
-            if _word in result:
-                result[_word] += 1
-            else:
-                result[_word] = 1
-
-        return result
+        return 0
 
     """
         @ Author:       Guillermo Rodriguez
         @ Date:         09/18/2018
-        @ Purpose:      Tokenize a data set
+        @ Purpose:      Calculate inverse document frequency (idf)
     """
-    def stemminization(self, tokenized_data):
-        result = {}
+    def idf(self, documents, word, stem):
 
-        stemmer = PorterStemmer()
+        _root_word = PorterStemmer().stem(WordNetLemmatizer().lemmatize(word))
+        _length = 0
+        _found_in = 0
 
-        for _token in tokenized_data:
-            _word = stemmer.stem(_token)
+        for document in documents.values():
+            _length += len(document.replace(' ', ''))
 
-            if _word in result:
-                result[_word] += 1
-            else:
-                result[_word] = 1
+        for entry in stem.values():
+            if _root_word in entry:
+                _found_in += 1
 
-        return result
+        if _found_in == 0:
+            _found_in = 1
+
+        return math.log(_length/_found_in)
--- tfidf.py
+++ tfidf.py
@@ -11,7 +11,7 @@ parser.add_argument('-statistics', help='Statistical Data Output [NO | YES]')
 parser.add_argument('-query', help='Query String')
 parse = parser.parse_args()
 
-if parse.graph and parse.statistics:
+if parse.graph and parse.statistics and parse.query:
 
     _path = os.getcwd() + '\\Data\\'
     _type = '.txt'
@@ -20,6 +20,9 @@ if parse.graph and parse.statistics:
     _token = {}
     _lem = {}
     _stem = {}
+    _tf = {}
+    _idf = {}
+    _tfidf = {}
 
     # Read Data set=
     for _file in os.listdir(_path):
@@ -37,9 +40,32 @@ if parse.graph and parse.statistics:
     for _doc in _content.keys():
         if len(_content[_doc].strip()) > 0:
             _token[_doc] = _statistics.tokenization(_content[_doc])
-            _lem[_doc] = _statistics.lemmatization(_token[_doc])
-            _stem[_doc] = _statistics.stemminization(_lem[_doc])
+            _lem[_doc] = _statistics.lemmatization(_content[_doc])
+            _stem[_doc] = _statistics.stemminization(_content[_doc])
 
+            for element in parse.query.split(' '):
+                _tf[_doc + '-' + element] = _statistics.tf(_stem[_doc], element)
+
+    for element in parse.query.split(' '):
+        _idf[element] = _statistics.idf(_content, element, _stem)
+
+    print("Tokens:")
+    print(_token)
+
+    print("Lemmatization:")
+    print(_lem)
+
+    print("Stemming:")
+    print(_stem)
+
+    print("Term Frequency:")
+    print(_tf)
+
+    print("Inverse Document Frequency")
+    print(_idf)
+
+    print("Term Frequency - Inverse Document Frequency")
+    print(_tfidf)
 
     # Create Graphical Elements
     _graph = graph()
